52dc0455ea9eea507a864ccd65d4ec6386cec093
Luca Zampetti <lzampetti@gmail.com>
2019-05-09 09:19:19 +0200

    atlas

--- src/app/contacts/contacts.controller.js
+++ src/app/contacts/contacts.controller.js
@@ -6,10 +6,12 @@ class ContactsCtrl {
 	constructor(
 		$scope,
 		$timeout,
+		$http,
 		StateService
 	) {
 		this.$scope = $scope;
 		this.$timeout = $timeout;
+		this.$http = $http;
 		this.data = window.data || {};
 		this.model = {};
 		this.state = StateService.getState();
@@ -19,14 +21,26 @@ class ContactsCtrl {
 	onSubmit() {
 		console.log('ContactsCtrl.onSubmit', this.model);
 		if (this.state.busy()) {
+			this.$http.post('/WS/wsForms.asmx/SaveForm', this.model).then(
+				success => {
+
+				},
+				error => {
+					this.error = error;
+				}
+			).finally(() => {
+				this.state.ready();
+			});
+			/*
 			this.$timeout(() => {
 				this.state.ready();
 			}, 2000);
+			*/
 		}
 	}
 
 }
 
-ContactsCtrl.$inject = ['$scope', '$timeout', 'StateService'];
+ContactsCtrl.$inject = ['$scope', '$timeout', '$http', 'StateService'];
 
 export default ContactsCtrl;
